a00ecaa990104ed1eb6435da5bfa55d54e6008ca
Christina Sohn <chsohn15@gmail.com>
2020-06-04 22:24:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,4 +11,13 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-  array.flatten
+  array.flatten()
+end
+
+def using_delet(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
